29220b2efed6492b073f29c30486a22050e97574
root <root@gnu34.netlab.fe.up.pt>
2018-10-02 12:29:52 +0100

    added more functions

--- Projeto/protocol.c
+++ Projeto/protocol.c
@@ -17,6 +17,11 @@
 #define SENDER 0
 #define RECEIVER 1
 
+#define BEGIN 0
+#define START_MESSAGE 1
+#define MESSAGE 2
+#define END 3
+
 int flag = 1;
 
 void attend()
@@ -25,50 +30,98 @@ void attend()
 }
 
 int llopen_Receiver(int fd)
-{
+{ 
+    char BCC1 = A_SENDER ^ C_UA;
+    char ua[5] = {FLAG, A_SENDER, C_UA, BCC1, FLAG};
 
+    // ler SET //
+
+    write_message(fd, ua);
+
+    return 0;
 }
 
 int llopen_Sender(int fd)
-{
-
-}
-
-int llopen(int fd, int flag)
 {
     (void) signal(SIGALRM, attend);
 
     char BCC1 = A_SENDER ^ C_SET;
     char set[5] = {FLAG, A_SENDER, C_SET, BCC1, FLAG};
 
-    if(flag == SENDER)
+    int cnt = 0;
+    char buf[255];
+
+    while(cnt < 3)
     {
-        llopen_Sender(fd);
+        alarm(3);
+        flag = 0;
+        
+        write_message(fd, buf);
+        // ler UA //
+
+        cnt++;
     }
-    else if (flag == RECEIVER)
-    {
-        llopen_Receiver(fd);
-    }
-    else
-    {
-        return -1;
-    }
+
 
     
 
+}
+
+int read_message(int fd, char buf[], int f)
+{
+    int state = BEGIN;
+
+    while(state != END)
+    {
+        if(FLAG != f) return -3;
+
+        switch(state)
+        {
+            case BEGIN: 
+            {
+                
+                break;
+            }
+            case START_MESSAGE:
+            {
+                break;
+            }            
+            case MESSAGE:
+            {
+                break;
+            }
+            default: state = END;
+        }
+    }
+
+    return 0;
+}
+
+int llopen(int fd, int flag)
+{    
+    if(flag == SENDER)
+    {
+        return llopen_Sender(fd);
+    }
+    else if (flag == RECEIVER)
+    {
+        return llopen_Receiver(fd);
+    }
+  
+    return -1;
 }
 
 int write_message(int fd, char buf[])
 {    
     if(gets(buf) == NULL)
     {
-    	exit(-1);
+    	return -2;
     }
 
     int res = write(fd,buf,255);   
     fflush(NULL);
 
-    printf("%d bytes written\n", res);
-
     sleep(1);
+
+    return 0;
 }
