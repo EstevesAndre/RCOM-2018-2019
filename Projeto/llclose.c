79d639523475a4667a7d055511790dd5032131c7
root <root@gnu34.netlab.fe.up.pt>
2018-10-24 14:48:24 +0100

    added llclose

---
+++ Projeto/llclose.c
@@ -0,0 +1,72 @@
+#include "protocol.h"
+#include "llclose.h"
+
+int llclose(int fd, int flag)
+{
+    if(flag == RECEIVER)
+        return llclose_receiver(fd);
+    else
+        return llclose_sender(fd);  
+
+    return -1;
+}
+
+int llclose_receiver(int fd)
+{
+    unsigned char BCC1 = A_RECEIVER ^ C_DISC;
+    unsigned char disc[6] = {FLAG, A_RECEIVER, C_DISC, BCC1, FLAG, '\0'};
+
+    unsigned char buf[255];
+
+    while(1)
+    {
+        if(read_message(fd,buf) == 0) break;
+    }
+
+    if(parseMessageType(buf) == C_DISC)
+        write_message(fd,disc,5);
+    else
+        return -5;
+
+
+    while(1)
+    {
+        if(read_message(fd,buf) == 0) break;
+    }
+
+    if(parseMessageType(buf) == C_UA)
+    {
+        close(fd);
+        return 0;
+    }
+    
+    return -6;
+}
+
+int llclose_sender(int fd)
+{
+    unsigned char BCC1 = A_SENDER ^ C_DISC;
+    unsigned char disc[6] = {FLAG, A_SENDER, C_DISC, BCC1, FLAG, '\0'};
+
+    write_message(fd,disc,5);
+
+    unsigned char buf[255];
+
+    while(1)
+    {
+        if(read_message(fd,buf) == 0) break;
+    }
+
+    BCC1 = A_SENDER ^ C_UA;
+    unsigned char ua[6] = {FLAG, A_SENDER, C_UA, BCC1, FLAG, '\0'};
+
+    if(parseMessageType(buf) == C_DISC)
+    {
+        write_message(fd,ua,5);
+        close(fd);
+        return 0;
+    }
+        
+        
+    return -5;       
+}
---
+++ Projeto/llclose.h
@@ -0,0 +1,5 @@
+int llclose(int fd, int flag);
+
+int llclose_sender(int fd);
+
+int llclose_receiver(int fd);
