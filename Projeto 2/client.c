69f5fc23c0fddd90a61f91af928098b362f6334c
luisdiogo98 <luisdiogo98@hotmail.com>
2018-12-06 09:44:38 +0000

    Added TCP connection

--- Projeto 2/client.c
+++ Projeto 2/client.c
@@ -4,6 +4,11 @@
 #include <sys/types.h>
 #include <netinet/in.h> 
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <strings.h>
+#include <string.h>
 #include "info.h"
 
 int parseInfo(char* cmd, struct Info* info)
@@ -119,12 +124,47 @@ int parseInfo(char* cmd, struct Info* info)
     return 0;
 }
 
+int parseFilename(struct Info* info)
+{
+    char* filename = strrchr(info->path, '/');
+
+    if(filename == NULL)
+    {
+        info->filename = info->path;
+    }
+    else
+    {
+        info->filename = (filename + 1);
+    }
+
+    return 0;
+}
+
 int getHostInfo(char* hostname, struct hostent** h)
 {
     if ((*h=gethostbyname(hostname)) == NULL) 
         return -1;
     
     return 0;
+}
+
+int connectTCP(char* addr, int port)
+{
+    int	sockfd;
+	struct sockaddr_in server_addr;
+	
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(addr);
+	server_addr.sin_port = htons(port);	
+    
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) 
+        return -1;
+        
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        return -2;
+    
+    return sockfd;
 }
 
 int main(int argc, char** argv)
@@ -143,6 +183,8 @@ int main(int argc, char** argv)
         return -2;
     }
 
+    parseFilename(&info);
+
     struct hostent *host;
 
     if(getHostInfo(info.hostname, &host) != 0)
@@ -151,8 +193,22 @@ int main(int argc, char** argv)
         return -3;
     }
 
-    printf("Username: %s\nPassword: %s\nHostname: %s\nPath: %s\n", info.user, info.password, info.hostname, info.path);
-    printf("Full Host Name: %s\n", host->h_name);
-    printf("Host IP: %s\n", inet_ntoa(*((struct in_addr *)host->h_addr)));    
+    char* addr_str = inet_ntoa(*((struct in_addr *)host->h_addr));
+
+    int sockfd = connectTCP(addr_str, PORT);
+
+    if(sockfd < 0)
+    {
+        printf("Error connecting to IP %s using port %d\n", addr_str, PORT);
+        return -4;
+    }
+
+    printf("Username: %s\n", info.user);
+    printf("Password: %s\n", info.password);     
+    printf("Path: %s\n", info.path);
+    printf("Filename: %s\n", info.filename);
+    printf("Hostname: %s\n", info.hostname);     
+    printf("Full Hostname: %s\n", host->h_name);
+    printf("Host IP: %s\n", addr_str);     
     return 0;
 }
--- Projeto 2/info.h
+++ Projeto 2/info.h
@@ -1,3 +1,8 @@
+/**
+*   Compilation: gcc -o download client.c
+*   Test: ./download ftp://anonymous:1@speedtest.tele2.net/1KB.zip
+*/
+
 #define BEGIN 0
 #define USER 1
 #define PASSWORD 2
@@ -5,9 +10,12 @@
 #define PATH 4
 #define END 5
 
+#define PORT 21
+
 struct Info {
     char hostname[100];
     char path[150];
+    char* filename;
     char user[50];
     char password[50];
 };
